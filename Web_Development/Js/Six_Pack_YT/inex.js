10b8713e2728c52281b1bc51f64abccebf1327b9
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-03-05 19:00:03 +0530

    some files added in web

---
+++ Web_Development/Js/Six_Pack_YT/inex.js
@@ -0,0 +1,11 @@
+let heading = document.getElementById("head");
+
+let s = "Hi ra puka!";
+
+
+let str = `My name is Nikhil`;
+
+let arr = [1,2,3];
+
+// heading.innerText= ` ${str} okay! I got it now`;
+heading.innerText= "okay! I got it now " + arr;
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,50 +1,70 @@
 #include <stdio.h>
-#include <conio.h>
 
-int a[20][20], reach[20], n;
+int merge(int arr[], int aux[], int left, int mid, int right) {
+    int i, j, k;
+    int invCount = 0;
 
-void dfs(int v) {
-    int i;
-    reach[v] = 1;
-    for (i = 1; i <= n; i++) {
-        if (a[v][i] && !reach[i]) {
-            printf("\n %d->%d", v, i);
-            dfs(i);
+    i = left;
+    j = mid;
+    k = left;
+
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            aux[k++] = arr[i++];
+        } else {
+            aux[k++] = arr[j++];
+            invCount += (mid - i);
         }
     }
+
+    while (i <= mid - 1) {
+        aux[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+        aux[k++] = arr[j++];
+    }
+
+    for (i = left; i <= right; i++) {
+        arr[i] = aux[i];
+    }
+
+    return invCount;
+}
+
+int mergeSort(int arr[], int aux[], int left, int right) {
+    int mid, invCount = 0;
+
+    if (right > left) {
+        mid = (left + right) / 2;
+
+        invCount = mergeSort(arr, aux, left, mid);
+        invCount += mergeSort(arr, aux, mid + 1, right);
+        invCount += merge(arr, aux, left, mid + 1, right);
+    }
+
+    return invCount;
+}
+
+int countInversions(int arr[], int n) {
+    int aux[n];
+    return mergeSort(arr, aux, 0, n - 1);
 }
 
 int main() {
-    int i, j, count = 0;
+    int n;
 
-    printf("\n Enter number of vertices:");
+    printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++) {
-        reach[i] = 0;
-        for (j = 1; j <= n; j++) {
-            a[i][j] = 0;
-        }
+    int arr[n];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    printf("\n Enter the adjacency matrix:\n");
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            scanf("%d", &a[i][j]);
+    printf("Number of inversions: %d\n", countInversions(arr, n));
 
-    dfs(1);
-
-    printf("\n");
-    for (i = 1; i <= n; i++) {
-        if (reach[i])
-            count++;
-    }
-
-    if (count == n)
-        printf("\n Graph is connected");
-    else
-        printf("\n Graph is not connected");
-
-    getch();
     return 0;
 }
