1944aa21ea00a16c9cdf312ee2ff1622f7f9d12d
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-03-11 09:12:29 +0530

    some files added in web

--- Web_Development/Js/Six_Pack_YT/inex.js
+++ Web_Development/Js/Six_Pack_YT/inex.js
@@ -1,11 +1,36 @@
-let heading = document.getElementById("head");
+// let heading = document.getElementById("head");
 
-let s = "Hi ra puka!";
+// let s = "Hi ra puka!";
 
 
-let str = `My name is Nikhil`;
+// let str = `My name is Nikhil`;
 
-let arr = [1,2,3];
+// let arr = [1,2,3];
 
-// heading.innerText= ` ${str} okay! I got it now`;
-heading.innerText= "okay! I got it now " + arr;
+// // heading.innerText= ` ${str} okay! I got it now`;
+// heading.innerText= "okay! I got it now " + arr;
+
+// let a = 20;
+
+// {
+//     a = 30;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+var semester = new Object;
+
+semester.subjects = 2;
+semester.faculty = 3;
+
+// const a = document.getElementById("head");
+
+// a.innerHTML(`The elements in the object are:`);
+// semester.forEach(key,value => {
+//     console.log(value,key);
+// });
+
+for(var i in semester){
+    console.log(semester[i]);
+}
